86f0759d7f1d12bec0ad8891aaa2ced561d02cfc
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-07 23:18:16 +0300

    created the forms

---
+++ app/main/forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class CommentForm(FlaskForm):
+
+    title = StringField('Comment Title',validators=[DataRequired()])
+    review = TextAreaField('Movie review', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+    username = StringField('Enter your username',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [DataRequired()])
+    submit = SubmitField('Sign Up')
+    
+from wtforms import ValidationError
+class RegistrationForm(FlaskForm):
+    # .......
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+        
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+    
+    
+class PitchForm(FlaskForm):
+    
+    title = StringField('Review title',validators=[DataRequired()])
+    review = TextAreaField('Movie review')
+submit = SubmitField('Submit')
