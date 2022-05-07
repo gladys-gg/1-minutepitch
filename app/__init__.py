43f82d1ecafe28bf32ef0e64f66a6607a7f963d8
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-07 22:40:38 +0300

    Edited the navbar

--- app/__init__.py
+++ app/__init__.py
@@ -8,14 +8,12 @@ def create_app(config_name):
         # Creating the app configurations
 
     app.config.from_object(config_options[config_name])
-    config_options[config_name].init_app(app)
+    # config_options[config_name].init_app(app)
 
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
 
     return app
